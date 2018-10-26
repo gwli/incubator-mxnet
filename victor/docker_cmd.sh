57529cfe3aefe7dc66f813b133c418bd8f3aedb8
victor <vili@nvidia.com>
2018-10-26 13:43:50 +0800

    fix typo

--- victor/docker_cmd.sh
+++ victor/docker_cmd.sh
@@ -1,3 +1,3 @@
 docker pull victorgwli/mxnet:pip-cuda-8.0
 
-nvidia-docker run --privileged  -e DISPLAY  --net=host --ipc=host -it -v $HOME/.Xautority:/home/nvidia/.Xautoority -v `pwd`/..:/opt/mxnet -v `pwd`/../example/neural-style:/home/nvidia/samples/neural-style  -v `pwd`/test.sh:/home/nvidia/samples/neural-style/test.sh victorgwli/mxnet:pip-cuda-8.0 /bin/bash
+nvidia-docker run --privileged  -e DISPLAY  --net=host --ipc=host -it -v $HOME/.Xauthority:/home/nvidia/.Xauthority -v `pwd`/..:/opt/mxnet -v `pwd`/../example/neural-style:/home/nvidia/samples/neural-style  -v `pwd`/test.sh:/home/nvidia/samples/neural-style/test.sh victorgwli/mxnet:pip-cuda-8.0 /bin/bash
